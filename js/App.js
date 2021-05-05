d701744b45901045fc60ec4fe7fbc9dd61ef0ff7
Ain <ain.e.muhammad@tintash.com>
2021-05-05 20:47:08 +0500

    Modified the code to run promises rather than using AJAX

--- js/App.js
+++ js/App.js
@@ -17,35 +17,69 @@
 // console.log(jess.__proto__)
 
 
-//Ajax Call
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries')
 
+
+// //Ajax Call
+// const countriesContainer = document.querySelector('.countries')
+
+// const getCountryData = function(country){
+//     const request = new XMLHttpRequest();
+//     request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`)
+//     request.send()
+    
+//     //Rendering First Country
+//     request.addEventListener('load', function(){
+//       const [data]=  JSON.parse(this.responseText);
+//       console.log(data)
+//       RenderHtml(data)
+//     //Rendering the first neighbour of that country
+//       const neighbor = data.borders[0]
+//       if(!neighbor)
+//         return
+//       const request2 = new XMLHttpRequest();
+//       request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbor}`)
+//       request2.send()
+//       request2.addEventListener('load', function(){
+//         const data2 = JSON.parse(this.responseText)
+//         console.log(data2)
+//         RenderHtml(data2, 'neighbour')
+//       })
+//     });
+    
+// }
+
+// //Render HTML code
+// function RenderHtml(data, className='' ){
+//     const html = `<article class="country ${className}">
+//     <img class="country__img" src="${data.flag}" />
+//     <div class="country__data">
+//       <h3 class="country__name">${data.name}</h3>
+//       <h4 class="country__region">${data.name}</h4>
+//       <h4 class="country__row"><span>Population: </span></h4><p>${data.population}</p>
+//       <h4 class="country__row"><span>Language: </span></h4><p>${data.languages[0].name}</p>
+//       <h4 class="country__row"><span>Currency: </span> </h4><p>${data.currencies[0].name}</p>
+//     </div>
+//   </article>`
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+// }
+
+// // getCountryData('portugal');
+// getCountryData('pakistan');
+
+
+
+//Promises
+const countriesContainer = document.querySelector('.countries')
 const getCountryData = function(country){
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`)
-    request.send()
-    
-    //Rendering First Country
-    request.addEventListener('load', function(){
-      const [data]=  JSON.parse(this.responseText);
-      console.log(data)
-      RenderHtml(data)
-    //Rendering the first neighbour of that country
-      const neighbor = data.borders[0]
-      if(!neighbor)
-        return
-      const request2 = new XMLHttpRequest();
-      request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbor}`)
-      request2.send()
-      request2.addEventListener('load', function(){
-        const data2 = JSON.parse(this.responseText)
-        console.log(data2)
-        RenderHtml(data2, 'neighbour')
-      })
-    });
-    
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`).then((res) => {
+    // console.log(res)
+    return res.json()
+  }).then((data) => { 
+    // console.log(data)
+    RenderHtml(data[0])
+  })
 }
+getCountryData('pakistan')
 
 //Render HTML code
 function RenderHtml(data, className='' ){
@@ -61,6 +95,3 @@ function RenderHtml(data, className='' ){
   </article>`
     countriesContainer.insertAdjacentHTML('beforeend', html);
 }
-
-// getCountryData('portugal');
-getCountryData('pakistan');
