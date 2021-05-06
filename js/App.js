ff24826cc8c4bbc1de9a4e6fcea4e91027c8f6d2
Ain <ain.e.muhammad@tintash.com>
2021-05-06 10:50:27 +0500

    Made async promises and handled error within them too

--- js/App.js
+++ js/App.js
@@ -48,55 +48,6 @@
     
 // }
 
-// //Render HTML code
-// function RenderHtml(data, className='' ){
-//     const html = `<article class="country ${className}">
-//     <img class="country__img" src="${data.flag}" />
-//     <div class="country__data">
-//       <h3 class="country__name">${data.name}</h3>
-//       <h4 class="country__region">${data.name}</h4>
-//       <h4 class="country__row"><span>Population: </span></h4><p>${data.population}</p>
-//       <h4 class="country__row"><span>Language: </span></h4><p>${data.languages[0].name}</p>
-//       <h4 class="country__row"><span>Currency: </span> </h4><p>${data.currencies[0].name}</p>
-//     </div>
-//   </article>`
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-// }
-
-// // getCountryData('portugal');
-// getCountryData('pakistan');
-
-
-
-//Promises
-const countriesContainer = document.querySelector('.countries')
-const btn = document.querySelector('.btn-country')
-const getCountryData = function(country){
-  //Country 1
-  fetch(`https://restcountries.eu/rest/v2/name/${country}`).then((res) => {
-    // console.log(res)
-    return res.json()
-  },
-  //Error Handling
-  (err) =>{
-    console.log("Error handling")
-  }
-  ).then((data) => {   
-    // console.log(data)
-    RenderHtml(data[0])
-    //Neighbor country
-    const neighbor = data[0].borders[0]
-    return fetch(`https://restcountries.eu/rest/v2/name/${neighbor}`)
-  }).then((res) => { // Chaining promises
-    return res.json()
-  }).then((data) => {
-    RenderHtml(data[0], 'neighbour')
-  })
-}
-btn.addEventListener('click', () =>{
-  getCountryData('pakistan')
-})
-
 //Render HTML code
 function RenderHtml(data, className='' ){
     const html = `<article class="country ${className}">
@@ -111,3 +62,57 @@ function RenderHtml(data, className='' ){
   </article>`
     countriesContainer.insertAdjacentHTML('beforeend', html);
 }
+
+// // getCountryData('portugal');
+// getCountryData('pakistan');
+
+
+
+// //Promises
+// const countriesContainer = document.querySelector('.countries')
+// const btn = document.querySelector('.btn-country')
+// const getCountryData = function(country){
+//   //Country 1
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`).then((res) => {
+//     // console.log(res)
+//     return res.json()
+//   },
+//   //Error Handling
+//   (err) =>{
+//     console.log("Error handling")
+//   }
+//   ).then((data) => {   
+//     // console.log(data)
+//     RenderHtml(data[0])
+//     //Neighbor country
+//     const neighbor = data[0].borders[0]
+//     return fetch(`https://restcountries.eu/rest/v2/name/${neighbor}`)
+//   }).then((res) => { // Chaining promises
+//     return res.json()
+//   }).then((data) => {
+//     RenderHtml(data[0], 'neighbour')
+//   })
+// }
+// btn.addEventListener('click', () =>{
+//   getCountryData('pakistan')
+// })
+
+
+//Async Promises
+const countriesContainer = document.querySelector('.countries')
+const btn = document.querySelector('.btn-country')
+
+const whereAmI = async (country) => {
+  try{
+    const res = await fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+  const data = await res.json()
+  RenderHtml(data[0])
+  }
+  catch(err){
+    console.log(err)
+  }
+}
+
+btn.addEventListener('click', () =>{
+  whereAmI('pakistan')
+})
